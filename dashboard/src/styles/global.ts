00d1220c4ee98812be792782d89444c636fb700c
Samuel Simões <samuelsimoes31@gmail.com>
2021-02-13 21:17:14 -0300

    Some global stiles edtied

--- dashboard/src/styles/global.ts
+++ dashboard/src/styles/global.ts
@@ -4,12 +4,18 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
   }
 
   body {
     background: #FAFAFA;
     font-family: Poppins;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body html #root {
+    height: 100%;
   }
 
   .App {
